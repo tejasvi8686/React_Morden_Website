b01b82525be886eade2bef0d2c4a9b7e5eccef9b
tejasvi8686 <tejasviraj8686@gmail.com>
2023-07-28 18:47:49 +0530

    added values component

---
+++ src/components/Values.jsx
@@ -0,0 +1,39 @@
+import Image from "../images/values.jpg";
+import SectionHead from "./SeactionHead";
+import { GiCutDiamond } from "react-icons/gi";
+import { values } from "../data";
+import Card from "../UI/Card";
+
+const Values = () => {
+  return (
+    <section className="values">
+      <div className="container values__container">
+        <div className="values__left">
+          <div className="values__image">
+            <img src={Image} alt="values Images" />
+          </div>
+        </div>
+        <div className="values__right">
+        <SectionHead icon={<GiCutDiamond/>} title="values"></SectionHead>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit dignissimos veritatis accusamus autem deleniti ex.
+        </p>
+        <div className="values__wrapper"> 
+        {
+           values.map(({id, icon, title, desc}) =>{
+            return <Card className="values__value">
+            <span>{icon}</span>
+            <h4>{title}</h4>
+               <small>{desc}</small>
+            </Card>
+           }) 
+        }
+        
+        </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Values;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./home.css";
 import MainHeader from "../../components/MainHeader";
 import Programs from "../../components/Programs";
+import Values from "../../components/Values";
 
 const Home = () => {
   return (
     <div>
       <MainHeader />
       <Programs />
+      <Values/>
     </div>
   );
 };
