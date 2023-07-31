58312adddabea82d3bb0d33cabde3e2fb695be77
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-01 00:01:01 +0530

    added gallery section

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from "react";
 import "./gallery.css";
+import HeaderImage from "../../images/header_bg_3.jpg";
+import Header from "../../components/Header";
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+  const galleryLength = 15;
+  const images = [];
 
-export default Gallery
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`));
+  }
+
+  return (
+    <div>
+      <Header title="Our Gallery" image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ratione
+        porro placeat impedit delectus voluptatibus repellendus.
+      </Header>
+
+      <section className="gallery">
+        <div className="container gallery__container">
+          {images.map((image, index) => {
+            return (
+              <article key={index}>
+                <img src={image} alt={`Gallery Image ${index + 1}`} />
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Gallery;
