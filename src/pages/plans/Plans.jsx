0e304c1a87027315e05e59cfb44c50a8dda3511d
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-01 01:15:44 +0530

    added plans component

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import"./plans.css"
-
+import "./plans.css";
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_4.jpg";
+import Card from "../../UI/Card";
+import { plans } from "../../data";
 
 const Plans = () => {
   return (
-    <div>Plans</div>
-  )
-}
+    <div>
+      <Header title="Membership Plans" image={HeaderImage}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab rem odio praesentium iure rerum ipsam aut hic soluta magnam sapiente.
+      </Header>
 
-export default Plans
+      <section className="plans">
+       <div className="container plans__container">
+       {
+         plans.map(({id, name, desc, price, features})=>{
+          return <Card key={id} className='plan'>
+          <h3>{name}</h3>
+          <small>{desc}</small>
+          <h1>{`${price}`}</h1><h2>/mo</h2>
+          <h4>Features</h4>
+          {
+            features.map(({feature,available},index)=>{
+              return<p key={index} className={!available ? 'disable' : ''}>{feature}</p>
+            } )
+          }
+          <button className="btn lg">Choose Plan</button>
+          </Card>
+         })
+       }
+       
+       </div>
+      </section>
+    </div>
+  );
+};
+
+export default Plans;
